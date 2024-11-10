868ba04f840cffe550bb727d81f941b8fd5dbb9d
madan <madanhk18@gmail.com>
2024-11-10 20:12:38 +0530

    some erroe

--- ATMSimulator.java
+++
@@ -1,8 +0,0 @@
-public class ATMSimulator {
-    public static void main(String[] args) {
-        // Initialize the bank account with an initial balance
-        BankAccount account = new BankAccount(1000.0); // Starting balance of 1000
-        ATM atm = new ATM(account);
-        atm.displayMenu();
-    }
-}
--- Task3.java
+++ Task3.java
@@ -1,10 +1,11 @@
 
+
 import java.util.Scanner;
 
-public class ATM {
-    private BankAccount account;
+ class ATMM {
+    private Bankaccount account;
 
-    public ATM(BankAccount account) {
+    public ATMM(Bankaccount account) {
         this.account = account;
     }
 
@@ -58,3 +59,51 @@ public class ATM {
     }
 }
 
+ class Bankaccount {
+    private double balance;
+
+    public Bankaccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    // Deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. Amount deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdraw method
+    public boolean withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+            return false;
+        } else if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount.");
+            return false;
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful. Amount withdrawn: " + amount);
+            return true;
+        }
+    }
+
+    // Check balance method
+    public double getBalance() {
+        return balance;
+    }
+}
+
+
+
+public class ATMSimulator {
+    public static void main(String[] args) {
+        // Initialize the bank account with an initial balance
+        Bankaccount account = new Bankaccount(1000.0); // Starting balance of 1000
+        ATMM atm = new ATMM(account);
+        atm.displayMenu();
+    }
+}
